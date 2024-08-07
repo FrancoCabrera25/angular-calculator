36b118734905f6040d9ea803ec766d66b17c4e37
Franco Cabrera <cabrera.franco@outlook.com>
2024-08-07 09:47:59 -0300

    init calculator

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calculator.component.html',
+
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CalculatorComponent {}
---
+++ src/app/calculator/pages/calculator-page/calculator-page.component.ts
@@ -0,0 +1,11 @@
+import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-page',
+  standalone: true,
+  imports: [CalculatorComponent],
+  templateUrl: './calculator-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class CalculatorPageComponent {}
